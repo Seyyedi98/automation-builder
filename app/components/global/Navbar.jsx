f0384f8604ff57c3859251eec4d222da3d0b9b70
Seyyedi <mr.seyyedi76@gmail.com>
2024-09-25 06:23:11 +0330

    initial commit

--- app/components/global/Navbar.jsx
+++ app/components/global/Navbar.jsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import Logo from "./Logo";
+import { MenuIcon } from "lucide-react";
 
 const Navbar = async () => {
   return (
@@ -33,6 +34,28 @@ const Navbar = async () => {
           </li>
         </ul>
       </nav>
+      <aside className="flex items-center gap-4">
+        <Link
+          href="/dashboard"
+          className="relative inline-flex h-10 overflow-hidden rounded-full p-[2px] 
+          focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2
+           focus:ring-offset-slate-50"
+        >
+          <span
+            className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] 
+          bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]"
+          />
+          <span
+            className="inline-flex h-full w-full cursor-pointer items-center justify-center
+           rounded-full bg-slate-950 px-3 py-1 text-sm font-medium text-white
+            backdrop-blur-3xl"
+          >
+            {/* Add user later */}
+            {true ? "Dashboard" : "Get Started"}
+          </span>
+        </Link>
+        <MenuIcon className="md:hidden" />
+      </aside>
     </header>
   );
 };
