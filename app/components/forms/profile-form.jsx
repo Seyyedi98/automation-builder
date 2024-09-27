aedd8ab0854950da20cf86ad8bf7cedcce10125d
Seyyedi <mr.seyyedi76@gmail.com>
2024-09-27 19:34:08 +0330

    create settig page and add basic form

---
+++ app/components/forms/profile-form.jsx
@@ -0,0 +1,81 @@
+"use client";
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { EditUserProfileSchema } from "@/lib/types";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { Loader2 } from "lucide-react";
+
+const ProfileForm = () => {
+  const [isLoading, setIsLoading] = useState(false);
+
+  const form = useForm({
+    mode: "onChange",
+    resolver: zodResolver(EditUserProfileSchema),
+    defaultValues: {
+      name: "",
+      email: "",
+    },
+  });
+
+  return (
+    <Form {...form}>
+      <form className="flex flex-col gap-6" onSubmit={() => {}}>
+        <FormField
+          disabled={isLoading}
+          control={form.control}
+          name="name"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel className="text-lg">User full name</FormLabel>
+              <FormControl>
+                <Input placeholder="Name" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel className="text-lg">User Email</FormLabel>
+              <FormControl>
+                <Input placeholder="Email" type="email" disabled {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <Button
+          disabled={isLoading}
+          type="submit"
+          className="self-start hover:bg-[#2F006B] dark:hover:text-white"
+        >
+          {isLoading ? (
+            <>
+              <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+              Saving
+            </>
+          ) : (
+            "Save user Settings"
+          )}
+        </Button>
+      </form>
+    </Form>
+  );
+};
+
+export default ProfileForm;
